f8f09bfa0e951ac65ca254fd9c402d17985f951c
Přemysl Fára <enif@email.cz>
2022-04-17 17:09:27 +0200

    Static files demo app.

---
+++ StaticFilesDemoApp/Program.cs
@@ -0,0 +1,34 @@
+﻿/* AppHostDemo - (C) 2022 Premysl Fara  */
+
+using Microsoft.Extensions.Logging;
+
+using AppHost.AppServer;
+
+
+var appServer = AppServerBuilder.Build(new AppServerOptions()
+{
+    Args = args,
+    Port = 7777,
+    UseHttpsRedirection = true,
+    UseStaticFiles = true,
+    WebRootPath = "../../../wwwroot/",
+});
+
+var logger = appServer.Logger;
+
+logger.LogInformation("Starting the app server...");
+
+await appServer.StartAsync();
+
+
+Console.WriteLine("Press ENTER to stop...");
+_ = Console.ReadLine();
+
+
+logger.LogInformation("Stopping the app server...");
+    
+var status = await appServer.StopAsync();
+    
+logger.LogInformation("Task status: {Status}", status);
+
+logger.LogInformation("DONE!");
