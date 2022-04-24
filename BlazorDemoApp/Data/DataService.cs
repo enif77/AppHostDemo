16bef17689c7ddec67d58c2b314dcd3ee7e254f8
enif77 <premysl.fara@gmail.com>
2022-04-24 13:47:50 +0200

    The DataService uses ILogger.

--- BlazorDemoApp/Data/DataService.cs
+++ BlazorDemoApp/Data/DataService.cs
@@ -9,15 +9,15 @@ using System.Text.Json;
 public class DataService
 {
     private readonly HttpClient _httpClient;
-    //private readonly ILogger _logger;
+    private readonly ILogger<DataService> _logger;
 
     public int CounterValue { get; set; }
 
 
-    public DataService(HttpClient httpClient /*, ILogger logger*/)
+    public DataService(HttpClient httpClient, ILogger<DataService> logger)
     {
         _httpClient = httpClient ?? throw new ArgumentNullException(nameof(httpClient));
-        //_logger = logger ?? throw new ArgumentNullException(nameof(logger));
+        _logger = logger ?? throw new ArgumentNullException(nameof(logger));
     }
 
     
@@ -29,42 +29,19 @@ public class DataService
         }
         catch (HttpRequestException ex)
         {
-            //_logger.LogError("An error occurred: {Message}", ex.Message);
-            Console.WriteLine("An error occurred: {0}", ex.Message);
+            _logger.LogError("An error occurred: {Message}", ex.Message);
         }
         catch (NotSupportedException ex)
         {
-            //_logger.LogError("The content type is not supported: {Message}", ex.Message);
-            Console.WriteLine("The content type is not supported: {0}", ex.Message);
+            _logger.LogError("The content type is not supported: {Message}", ex.Message);
         }
         catch (JsonException ex)
         {
-            //_logger.LogError("Invalid JSON: {Message}", ex.Message);
-            Console.WriteLine("Invalid JSON: {0}", ex.Message);
+            _logger.LogError("Invalid JSON: {Message}", ex.Message);
         }
 
         return Array.Empty<WeatherForecast>();
     }
-    
-
-    // private static readonly string[] Summaries = new[]
-    // {
-    //     "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-    // };
-    //
-    //
-    // public Task<WeatherForecast[]> GetForecastAsync(DateTime startDate)
-    // {
-    //     var rng = new Random();
-    //     
-    //     return Task.FromResult(Enumerable.Range(1, 5).Select(index => new WeatherForecast
-    //         {
-    //             Date = startDate.AddDays(index),
-    //             TemperatureC = Random.Shared.Next(-20, 55),
-    //             Summary = Summaries[rng.Next(Summaries.Length)]
-    //         })
-    //         .ToArray());
-    // }
 }
 
 // http://localhost:7777/WeatherForecast
